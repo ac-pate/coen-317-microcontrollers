d1fa332a688c19a9ef5dbd86429344b61b4973dd
ac_pate <achalypatel3403@gmail.com>
2026-03-08 00:02:18 -0500

    adds lab1, lab2

---
+++ lab1/_impact.cmd
@@ -0,0 +1,29 @@
+setMode -bs
+setMode -bs
+setMode -bs
+setCable -port auto
+Identify -inferir 
+identifyMPM 
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+saveProjectFile -file "/nfs/home/a/ac_pate/COEN317/lab1//auto_project.ipf"
+setMode -bs
+setMode -bs
+deleteDevice -position 1
+deleteDevice -position 1
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
